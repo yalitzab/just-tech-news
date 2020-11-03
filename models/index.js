b1b19056bf7bd5fd867461b68a2725fa55fac4d4
yalitzab <yalitzabueno@gmail.com>
2020-11-02 20:56:13 -0800

    create and manipulate posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require("./User");
 const Post = require("./Post");
+const { userInfo } = require("os");
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
 
 module.exports = { User, Post };
