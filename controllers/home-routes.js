9b653935e689014b262c9be63fdbc66dd675035c
yalitzab <yalitzabueno@gmail.com>
2020-11-07 16:46:16 -0800

    add handlebars and html

---
+++ controllers/home-routes.js
@@ -0,0 +1,46 @@
+const models = require('../models');
+
+// This file will contain all of the user-facing routes, such as the homepage and login page.
+const router = require('express').Router();
+
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        // res.render('homepage', dbPostData[0].get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
-const { userInfo } = require("os");
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
